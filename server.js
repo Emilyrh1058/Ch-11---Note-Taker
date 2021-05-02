e2b764af1217373572b24dc05ad543ad22825e07
Emily Salinas <emilyrh1058@gmail.com>
2021-05-02 13:03:21 -0500

    GET and POST functionality fixed

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const { query } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express();
 const { notes } = require('./Develop/db/db.json');
+const app = express();
+const fs = require('fs');
+const path = require('path');
 
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 function filterByQuery(query, notesArray) {
@@ -21,6 +26,25 @@ function filterByQuery(query, notesArray) {
   return filteredResults;
 }
 
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './Develop/db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
 
 app.get('/api/notes', (req, res) => {
   let results = notes;
@@ -30,14 +54,17 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 });
 
-app.get('/api/notes/:1d', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
+app.post('/api/notes', (req, res) => {
+ // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
   } else {
-    res.sendStatus(404);
-  }  
-  res.json(404);
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 
