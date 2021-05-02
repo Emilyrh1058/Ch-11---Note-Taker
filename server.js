c19c68e56330a08a480aa3c26e64854d8e763d7b
Emily Salinas <emilyrh1058@gmail.com>
2021-05-02 11:16:52 -0500

    add PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { query } = require('express');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./Develop/db/db.json');
 
@@ -28,6 +29,6 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
