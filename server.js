5d1a406c5c6d71db8a11e62fc01ba4ab20406072
Emily Salinas <emilyrh1058@gmail.com>
2021-05-02 17:30:39 -0500

    ..

--- server.js
+++ server.js
@@ -1,85 +1,27 @@
-const { query } = require('express');
+// const { query } = require('express');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const { notes } = require('./Develop/db/db.json');
 const app = express();
-const fs = require('fs');
-const path = require('path');
 
-// parse incoming string or array data
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes')
+
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-
 app.use(express.static('public'));
 
 
-function filterByQuery(query, notesArray) {
-  let filteredResults = notesArray;
-  if (query.id) {
-    filteredResults = filteredResults.filter(note => note.id === query.id);
-  }
-  if (query.title) {
-    filteredResults = filteredResults.filter(note => note.title === query.title);
-  }
-  if (query.text) {
-    filteredResults = filteredResults.filter(note => note.text === query.text);
-  }
-  return filteredResults;
-}
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-function createNewNote(body, notesArray) {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, './Develop/db/db.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
-  return note;
-}
 
-function validateNote(note) {
-  if (!note.title || typeof note.title !== 'string') {
-    return false;
-  }
-  if (!note.text || typeof note.text !== 'string') {
-    return false;
-  }
-  return true;
-}
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('./Develop/db/db.json');
 
-app.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, notes);
-  }
-  res.json(results);
-});
-
-app.post('/api/notes', (req, res) => {
- // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-  } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
